88e09a8f7e5041245c2dcdcba6ebb7ae9a31c321
BAILLOT STEPHANE <[stefbbst@gmail.com](stefbbst@gmail.com)>
2023-04-27 11:15:16 +0200

    add IngredientController entity Ingredient & IngredientRepository

---
+++ src/main/java/com/wcs/project3/entity/Ingredient.java
@@ -0,0 +1,55 @@
+package com.wcs.project3.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
+    private Long id;
+    private String name;
+    private String logo;
+
+    @OneToMany(mappedBy = "ingredient", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<RecipeIngredient> recipes;
+
+    public Ingredient() { }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    public List<RecipeIngredient> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(List<RecipeIngredient> recipes) {
+        this.recipes = recipes;
+    }
+}
+
